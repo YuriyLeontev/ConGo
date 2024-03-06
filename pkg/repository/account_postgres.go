3c077e2c5bff7b18251139d72ff4250ae9f93f78
YuriyLeontev <etufkti@gmail.com>
2024-03-06 15:27:50 +0300

    Добавил обработку запросов

--- pkg/repository/account_postgres.go
+++ pkg/repository/account_postgres.go
@@ -43,7 +43,7 @@ func (r *AccountsPostgres) Filter(filters []congo.Filter, limit int) ([]congo.Ac
 			if filter.Method == "domain" {
 				request_filter = append(request_filter, " email ILIKE  '%"+fmt.Sprintf("%s' ", filter.Parametr[0]))
 			} else if filter.Method == "lt" {
-				request_filter = append(request_filter, fmt.Sprint(" email < '%s' ", filter.Parametr[0]))
+				request_filter = append(request_filter, fmt.Sprintf(" email < '%s' ", filter.Parametr[0]))
 			} else if filter.Method == "gt" {
 				request_filter = append(request_filter, fmt.Sprintf(" email > '%s' ", filter.Parametr[0]))
 			}
@@ -85,14 +85,76 @@ func (r *AccountsPostgres) Filter(filters []congo.Filter, limit int) ([]congo.Ac
 				request_filter = append(request_filter, fmt.Sprintf("sname ILIKE '%s", filter.Parametr[0])+"%'")
 			} else if filter.Method == "null" {
 				if filter.Parametr[0] == "0" {
-					// Указано имя
+					// Указана фамилия
 					request_filter = append(request_filter, "sname IS NULL")
 				} else if filter.Parametr[0] == "1" {
-					// Не указано имя
+					// Не указана фамилия
 					request_filter = append(request_filter, "sname IS NOT NULL")
 				}
 			}
 		}
+		if strings.Compare(filter.Filter, "phone") == 0 {
+			if filter.Method == "code" {
+				request_filter = append(request_filter, " phone ILIKE '%("+filter.Parametr[0]+")%'")
+			} else if filter.Method == "null" {
+				if filter.Parametr[0] == "0" {
+					// Указан номер
+					request_filter = append(request_filter, "phone IS NULL")
+				} else if filter.Parametr[0] == "1" {
+					// Не указан номер
+					request_filter = append(request_filter, "phone IS NOT NULL")
+				}
+			}
+		}
+		if strings.Compare(filter.Filter, "country") == 0 {
+			if filter.Method == "eq" {
+				request_filter = append(request_filter, fmt.Sprintf("country = '%s'", filter.Parametr[0]))
+			} else if filter.Method == "null" {
+				if filter.Parametr[0] == "0" {
+					// Указана страна
+					request_filter = append(request_filter, "country IS NULL")
+				} else if filter.Parametr[0] == "1" {
+					// Не указана страна
+					request_filter = append(request_filter, "country IS NOT NULL")
+				}
+			}
+		}
+		if strings.Compare(filter.Filter, "city") == 0 {
+			if filter.Method == "eq" {
+				request_filter = append(request_filter, fmt.Sprintf("city = '%s'", filter.Parametr[0]))
+			} else if filter.Method == "any" {
+				cities := "("
+				for i, city := range filter.Parametr {
+					if i != 0 {
+						cities += ", "
+					}
+					cities += fmt.Sprintf("'%s'", city)
+				}
+				cities += ")"
+				request_filter = append(request_filter, fmt.Sprintf(" city IN %s", cities))
+			} else if filter.Method == "null" {
+				if filter.Parametr[0] == "0" {
+					// Указан город
+					request_filter = append(request_filter, "city IS NULL")
+				} else if filter.Parametr[0] == "1" {
+					// Не указан город
+					request_filter = append(request_filter, "city IS NOT NULL")
+				}
+			}
+		}
+		if strings.Compare(filter.Filter, "birth") == 0 {
+			if filter.Method == "lt" {
+				request_filter = append(request_filter, fmt.Sprintf(" birth < '%s'", filter.Parametr[0]))
+			} else if filter.Method == "gt" {
+				request_filter = append(request_filter, fmt.Sprintf(" birth > '%s' ", filter.Parametr[0]))
+			} else if filter.Method == "year" {
+				request_filter = append(request_filter, fmt.Sprintf(" birth BETWEEN '01.01.%s' AND '12.31.%s' ", filter.Parametr[0], filter.Parametr[0]))
+			}
+		}
+
+		// TO-DO interests (contains, any)
+		// TO-DO likes (contains)
+		// TO-DO premium (now, null)
 	}
 
 	where := ""
